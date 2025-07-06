e99c0ce9082315af7335befdeb5589486920c49e
Maxwell The Cat <105382091+JustASpoonful@users.noreply.github.com>
2025-07-05 21:23:25 -0400

    Create content.js

---
+++ ProjectPuffin/content.js
@@ -0,0 +1,3 @@
+(function(){    
+  var script = document.createElement('script');    
+  script.src = 'https://homelesspigeon.vercel.app/core/pigeonbrain.js'; document.body.appendChild(script);})();
